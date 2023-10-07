315e9a8a54e4119800726feb131d7fdabd79fb17
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-10-07 17:14:40 +0700

    Bookings: Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { getBookings as getBookingsApi } from "../../services/apiBookings.js";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const { isLoading, data: bookings } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookingsApi,
+    queryKey: ["bookings", filter], // refresh page if bookings / filter changed
+    queryFn: () => getBookingsApi({ filter }),
   });
   return { isLoading, bookings };
 }
