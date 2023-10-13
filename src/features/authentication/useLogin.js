c430e2605fc085b8405697c38d7679a1e34f75ab
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-10-13 15:36:19 +0700

    Authentication: Keep Login When Page Reloaded

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth.js";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading: isLoadingLogin } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user); // set data into react query catch
       navigate("/");
     },
     onError: (err) => {
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -8,5 +8,21 @@ export async function login({ email, password }) {
 
   if (error) throw new Error(error.message);
 
+  console.log("login", data);
+
   return data;
+}
+
+export async function getCurrentUser() {
+  // get access session token from  local storage
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  // get user from supabase
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
 }
